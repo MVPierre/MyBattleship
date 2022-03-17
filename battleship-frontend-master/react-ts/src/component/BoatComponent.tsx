94c2ce8670d677a5633b358ee0a36330ef474f6b
Pierre MARTIN-VILLEPOU <pierre.martin-villepou@ixxitechside.com>
2022-03-17 15:29:17 +0100

    Init Analyse Page

--- battleship-frontend-master/react-ts/src/component/BoatComponent.tsx
+++ battleship-frontend-master/react-ts/src/component/BoatComponent.tsx
@@ -7,7 +7,7 @@ export default class BoatComponent extends React.Component<Boat, {}> {
     render() {
         return (
             <div className='boatInformation'>
-                <img alt='boat' src={boat}/>
+                <img alt='boats' src={boat}/>
                 <strong>{this.props.name}</strong>{' '}
                 {this.props.sunk ? 'SUNK' : (this.props.touched ? 'TOUCHED' : 'UNTOUCHED') }
             </div>
